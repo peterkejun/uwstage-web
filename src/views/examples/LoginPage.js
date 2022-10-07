e2da44e85d30a2e0d90d138e4f5344bf412bf2e6
Peter Ke <j5ke@uwaterloo.ca>
2022-10-07 03:00:16 -0400

    login page

---
+++ src/views/examples/LoginPage.js
@@ -0,0 +1,217 @@
+/*!
+
+=========================================================
+* BLK Design System React - v1.2.1
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/blk-design-system-react
+* Copyright 2022 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/blk-design-system-react/blob/main/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+import classnames from "classnames";
+// reactstrap components
+import {
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  CardImg,
+  CardTitle,
+  Label,
+  FormGroup,
+  Form,
+  Input,
+  InputGroupAddon,
+  InputGroupText,
+  InputGroup,
+  Container,
+  Row,
+  Col
+} from "reactstrap";
+
+// core components
+import ExamplesNavbar from "components/Navbars/ExamplesNavbar.js";
+import Footer from "components/Footer/Footer.js";
+import textManager from "string/TextManager";
+import apiClient from "api/ApiClient";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+
+export default function LoginPage() {
+    const history = useHistory();
+  const [squares1to6, setSquares1to6] = React.useState("");
+  const [squares7and8, setSquares7and8] = React.useState("");
+  const [fullNameFocus, setFullNameFocus] = React.useState(false);
+  const [emailFocus, setEmailFocus] = React.useState(false);
+  const [passwordFocus, setPasswordFocus] = React.useState(false);
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  React.useEffect(() => {
+    document.body.classList.toggle("login-page");
+    document.documentElement.addEventListener("mousemove", followCursor);
+    // Specify how to clean up after this effect:
+    return function cleanup() {
+      document.body.classList.toggle("login-page");
+      document.documentElement.removeEventListener("mousemove", followCursor);
+    };
+  }, []);
+  const followCursor = (event) => {
+    let posX = event.clientX - window.innerWidth / 2;
+    let posY = event.clientY - window.innerWidth / 6;
+    setSquares1to6(
+      "perspective(500px) rotateY(" +
+        posX * 0.05 +
+        "deg) rotateX(" +
+        posY * -0.05 +
+        "deg)"
+    );
+    setSquares7and8(
+      "perspective(500px) rotateY(" +
+        posX * 0.02 +
+        "deg) rotateX(" +
+        posY * -0.02 +
+        "deg)"
+    );
+  };
+  const handleSubmit = async (e) => {
+    const result = await apiClient.login(email, password);
+    if (result) {
+        history.push('/search-page');
+    }
+  }
+  return (
+    <>
+      <ExamplesNavbar />
+      <div className="wrapper">
+        <div className="page-header">
+          <div className="page-header-image" />
+          <div className="content">
+            <Container>
+              <Row>
+                <Col className="offset-lg-0 offset-md-3" lg="5" md="6">
+                  <div
+                    className="square square-7"
+                    id="square7"
+                    style={{ transform: squares7and8 }}
+                  />
+                  <div
+                    className="square square-8"
+                    id="square8"
+                    style={{ transform: squares7and8 }}
+                  />
+                  <Card className="card-register">
+                    <CardHeader>
+                      <CardImg
+                        alt="..."
+                        src={require("assets/img/square-purple-1.png")}
+                      />
+                      <CardTitle tag="h4">{textManager.getText('LOGIN')}</CardTitle>
+                    </CardHeader>
+                    <CardBody>
+                      <Form className="form" onSubmit={handleSubmit}>
+                        <InputGroup
+                          className={classnames({
+                            "input-group-focus": emailFocus
+                          })}
+                        >
+                          <InputGroupAddon addonType="prepend">
+                            <InputGroupText>
+                              <i className="tim-icons icon-email-85" />
+                            </InputGroupText>
+                          </InputGroupAddon>
+                          <Input
+                            placeholder="Email"
+                            type="text"
+                            onFocus={(e) => setEmailFocus(true)}
+                            onBlur={(e) => setEmailFocus(false)}
+                            onChange={(e) => setEmail(e.target.value)}
+                          />
+                        </InputGroup>
+                        <InputGroup
+                          className={classnames({
+                            "input-group-focus": passwordFocus
+                          })}
+                        >
+                          <InputGroupAddon addonType="prepend">
+                            <InputGroupText>
+                              <i className="tim-icons icon-lock-circle" />
+                            </InputGroupText>
+                          </InputGroupAddon>
+                          <Input
+                            placeholder="Password"
+                            type="text"
+                            onFocus={(e) => setPasswordFocus(true)}
+                            onBlur={(e) => setPasswordFocus(false)}
+                            onChange={(e) => setPassword(e.target.value)}
+                          />
+                        </InputGroup>
+                        <FormGroup check className="text-left">
+                          <Label check>
+                            <Input type="checkbox" />
+                            <span className="form-check-sign" />I agree to the{" "}
+                            <a
+                              href="#pablo"
+                              onClick={(e) => e.preventDefault()}
+                            >
+                              terms and conditions
+                            </a>
+                            .
+                          </Label>
+                        </FormGroup>
+                      </Form>
+                    </CardBody>
+                    <CardFooter>
+                      <Button className="btn-round" color="primary" size="lg" onClick={handleSubmit}>
+                        Get Started
+                      </Button>
+                    </CardFooter>
+                  </Card>
+                </Col>
+              </Row>
+              <div className="register-bg" />
+              <div
+                className="square square-1"
+                id="square1"
+                style={{ transform: squares1to6 }}
+              />
+              <div
+                className="square square-2"
+                id="square2"
+                style={{ transform: squares1to6 }}
+              />
+              <div
+                className="square square-3"
+                id="square3"
+                style={{ transform: squares1to6 }}
+              />
+              <div
+                className="square square-4"
+                id="square4"
+                style={{ transform: squares1to6 }}
+              />
+              <div
+                className="square square-5"
+                id="square5"
+                style={{ transform: squares1to6 }}
+              />
+              <div
+                className="square square-6"
+                id="square6"
+                style={{ transform: squares1to6 }}
+              />
+            </Container>
+          </div>
+        </div>
+        <Footer />
+      </div>
+    </>
+  );
+}
